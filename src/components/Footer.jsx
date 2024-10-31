fe163c579a4ed323871c976bf68d3c968a9a42e4
hasanali207 <hasanali142228@gmail.com>
2024-10-31 13:31:18 +0600

    added data with changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,13 @@ import {  FaGithub, FaLinkedin, FaLinkedinIn } from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div>
-
+    
 <footer className=" mt-20 footer footer-center  text-base-content rounded p-10">
   <nav className="grid grid-flow-col text-lg gap-4">
     <a href='/' className="link link-hover text-slate-600 uppercase">Home</a>
-    <a href='/contact' className="link link-hover text-slate-600  uppercase">Contact</a>
-    <a href='/about' className="link link-hover text-slate-600  uppercase">About</a>
-    <a href='/portfolio' className="link link-hover text-slate-600  uppercase">Portfolio</a>
+    <a href='#contact' className="link link-hover text-slate-600  uppercase">Contact</a>
+    <a href='#about' className="link link-hover text-slate-600  uppercase">About</a>
+    <a href='#skills' className="link link-hover text-slate-600  uppercase">Skills</a>
   </nav>
   <nav>
     
@@ -29,7 +28,7 @@ const Footer = () => {
   </aside>
 </footer>
 
-    </div>
+    
   )
 }
 
