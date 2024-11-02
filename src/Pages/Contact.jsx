e3eb8eaa488418833a495c1459e5a8d5b3d639f3
hasanali207 <hasanali142228@gmail.com>
2024-11-02 13:04:11 +0600

    added data with changed

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -133,15 +133,7 @@ const Contact = () => {
                 <span>+8801861026837</span>
               </p>
               <p className="flex items-center text-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 mr-2 sm:mr-6"
-                >
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                </svg>
+                <div className="icon"><FaGithub></FaGithub></div>
                 <span>hasandevpro@gmail.com</span>
               </p>
 
