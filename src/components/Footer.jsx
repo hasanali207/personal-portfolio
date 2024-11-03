6f93cbdcaf69cc17e661e0a1a107c6aab9f8b8ae
hasanali207 <hasanali142228@gmail.com>
2024-11-03 19:11:41 +0600

    added data with changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,27 @@
 import { FaChevronUp } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+import logo from '../assets/logo1.png';
 const Footer = () => {
   const navitems = (
     <>
      
-      <li className="mr-3 text-base text-black ">
+      <li className="mr-3 text-lg text-white-700 ">
         <NavLink className={({ isActive }) =>
               isActive ? 'text-[#757575] focus:text-[#757575] ' : 'hover:text-[#757575]'
             } to="/portfolio">Portfolio</NavLink>
       </li>
-      <li className="mr-3 text-base text-black">
+      <li className="mr-3 text-lg  ">
         <NavLink className={({ isActive }) =>
               isActive ? 'text-[#757575] focus:text-[#757575]' : 'hover:text-[#757575]'
             } to="/timeline">Timeline</NavLink>
       </li>
-      <li className="mr-3 text-base text-black">
+      <li className="mr-3 text-lg text-white-700 hover:text-[#757575]">
         <NavLink className={({ isActive }) =>
               isActive ? 'text-[#757575] focus:text-[#757575]' : 'hover:text-[#757575]'
             } to="/about">About</NavLink>
       </li>
      
-      <li className="mr-3 text-base text-black">
+      <li className="mr-3 text-lg text-white-700 hover:text-[#757575]">
         <NavLink className={({ isActive }) =>
               isActive ? 'text-[#757575] focus:text-[#757575]' : 'hover:text-[#757575]'
             } to="/contact">Contact</NavLink>
@@ -40,9 +40,11 @@ const Footer = () => {
       <FaChevronUp />
     </a>
   </div>
-  <div className="footer-content">
-    <p>© 2024 Your HasanDevpro</p>
-    <ul className="footer-nav">
+  <div className="flex justify-center items-center flex-col space-y-7">
+
+  <Link to={'/'} className=" "> <img className='w-24 h-24' src={logo} alt="" />   </Link>
+    <p className="text-[#ddd]">© 2024 Your HasanDevpro</p>
+    <ul className="flex justify-between items-center text-[#ddd]">
       {navitems}
     </ul>
   </div>
