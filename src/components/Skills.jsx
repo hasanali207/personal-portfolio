804b5f881fee1f9219ae2089f674bfa78d905d73
hasanali207 <hasanali142228@gmail.com>
2024-11-07 12:25:45 +0600

    added data with changed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,16 +4,17 @@ import img3 from '../assets/tailwind.png';
 import img4 from '../assets/bootstrap.png';
 import img5 from '../assets/react.png';
 import img6 from '../assets/node.png';
-import img7 from '../assets/wordpress.png';
-import img8 from '../assets/js.png';
-import img9 from '../assets/figma.png';
-import img10 from '../assets/react.png';
+import img7 from '../assets/mongodb.png';
+import img8 from '../assets/wordpress.png';
+import img9 from '../assets/js.png';
+import img10 from '../assets/figma.png';
+import img11 from '../assets/react.png';
 import Marquee from 'react-fast-marquee';
 
 
 export default function Skills() {
   return (
-    <div className='bg-[#0a192f] py-20 h-full lg:h-[100vh]'>
+    <div className='bg-[#0a192f] py-20 '>
       <div className='container mx-auto'> 
      <div className="flex flex-col justify-center items-center mb-10">
       
@@ -21,7 +22,7 @@ export default function Skills() {
               <p className='text-[#ddd] text-center'>We put your ideas and thus your wishes in the form of a unique web project that <br/> inspires you and you customers.</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-2">
+      <div className="flex  items-center flex-wrap gap-4">
 
       {/* item1 */} 
       <div className="flex flex-col justify-center items-center skill-item">
@@ -77,10 +78,10 @@ export default function Skills() {
       <div className="flex flex-col justify-center items-center skill-item">
         
         <div className='icon-box'>
-          <img className="w-20 h-20" src={img9} alt="HTML" />
+          <img className="w-20 h-20" src={img7} alt="MongoDB" />
           
         </div>
-        <p className="text-lg mt-4 text-[#8750f7]">Figma</p>
+        <p className="text-lg mt-4 text-[#8750f7]">MONGODB</p>
                
       </div>
 
@@ -89,6 +90,78 @@ export default function Skills() {
 
       </div>
      
+    
+     <Marquee>
+
+     
+      <div className="flex  items-center flex-wrap gap-4 mt-6">
+
+      {/* item1 */} 
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img1} alt="HTML" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">HTML</p>
+               
+      </div>
+      {/* item2 */}
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img2} alt="CSS3" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">CSS3</p>
+               
+      </div>
+      {/* item3 */}
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img3} alt="Tailwind" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">TAILWIND</p>
+               
+      </div>
+    {/* item  */}
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img5} alt="HTML" />
+          
+        </div>
+        <p className="text-lg mt-4  text-[#8750f7] ">React</p>
+               
+      </div>
+      {/* item */}
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img6} alt="HTML" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">Node</p>
+               
+      </div>
+    
+      <div className="flex flex-col justify-center items-center skill-item">
+        
+        <div className='icon-box'>
+          <img className="w-20 h-20" src={img7} alt="MongoDB" />
+          
+        </div>
+        <p className="text-lg mt-4 text-[#8750f7]">MONGODB</p>
+               
+      </div>
+
+
+
+
+      </div>
+      </Marquee>
 
 
 
